086647cfadb94d058429b6a0ec139408ebdd69eb
Minjeong-Kim <alswjd240202@gmail.com>
2025-08-20 11:10:15 +0900

    2025-08-20 조건에맞는사원정보_풀이추가

--- 프로그래머스_LV2/조건에맞는사원정보조회하기.sql
+++ 프로그래머스_LV2/조건에맞는사원정보조회하기.sql
@@ -28,3 +28,33 @@ ORDER BY
     SCORE DESC
 LIMIT 1
 ;
+
+-- 최고점이 동일한 사원이 여러명 있었다면??
+SELECT 
+    SUM(G.SCORE) AS SCORE,
+    E.EMP_NO,
+    E.EMP_NAME,
+    E.POSITION,
+    E.EMAIL
+FROM
+    HR_EMPLOYEES AS E
+JOIN 
+    HR_GRADE AS G USING(EMP_NO)    
+WHERE 
+    G.YEAR = '2022'
+GROUP BY
+    EMP_NO   
+HAVING -- 3. 사원의 2022 점수합계가 '최고점수'와 동일한 경우만 필터
+    SUM(G.SCORE) = (SELECT 
+                        MAX(SUM_SCORE) AS MAX_SUM -- 2. 사원별 점수합계에서 '최고점수'만 선택
+                    FROM (SELECT
+                                EMP_NO,
+                                SUM(SCORE) AS SUM_SCORE
+                            FROM
+                                HR_GRADE 
+                            WHERE 
+                                YEAR = '2022'
+                            GROUP BY
+                                EMP_NO) AS M_SCORE) -- 1. HR_GRADE 테이블에서 2022년, 사원별 점수합계 구하고
+;
+    
