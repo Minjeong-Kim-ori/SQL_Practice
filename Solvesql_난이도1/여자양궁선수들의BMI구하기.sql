26eb30efe1c764ffb744565f0cd9394c6e14ca77
Minjeong-Kim <alswjd240202@gmail.com>
2025-09-15 12:48:30 +0900

    2025-09-15 solvesql

---
+++ Solvesql_난이도1/여자양궁선수들의BMI구하기.sql
@@ -0,0 +1,14 @@
+SELECT
+    athlete_id,
+    medal,
+    sex,
+    weight,
+    height,
+    ROUND(weight / POWER((height * 0.01),2),2) AS bmi
+FROM
+    records
+WHERE
+    game_id = 49 AND event_id = 39 
+    AND weight IS NOT NULL 
+    AND height IS NOT NULL
+;
