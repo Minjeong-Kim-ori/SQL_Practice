07beef124e3221ae979ecd606d96e0171eb9f5cd
Minjeong-Kim <alswjd240202@gmail.com>
2025-10-21 15:41:58 +0900

    2025-10-21 programmers

---
+++ 프로그래머스_ver2/LV.1/가장큰물고기10마리구하기.sql
@@ -0,0 +1,11 @@
+SELECT
+    id,
+    length
+FROM
+    fish_info
+WHERE
+    length IS NOT NULL
+ORDER BY
+    length DESC, id 
+LIMIT 10
+;
---
+++ 프로그래머스_ver2/LV.1/조건에부합하는중고거래댓글조회하기.sql
@@ -0,0 +1,18 @@
+SELECT
+    title,
+    b.board_id,
+    reply_id,
+    r.writer_id,
+    r.contents,
+    DATE_FORMAT(r.created_date, '%Y-%m-%d') AS created_date
+FROM
+    used_goods_board AS b
+JOIN
+    used_goods_reply AS r
+ON
+    b.board_id = r.board_id
+WHERE
+    b.created_date LIKE '2022-10%'
+ORDER BY
+    r.created_date, title
+;
