d09af6f648b2105b877b184fa96dd62c102ac31e
Minjeong-Kim <alswjd240202@gmail.com>
2025-11-10 22:56:31 +0900

    2025-11-10 programmers

---
+++ 프로그래머스_ver2/LV.3/대여기록이존재하는자동차리스트구하기.sql
@@ -0,0 +1,15 @@
+SELECT DISTINCT
+    c.car_id
+FROM
+    car_rental_company_car AS c
+JOIN
+    car_rental_company_rental_history AS h
+ON
+    c.car_id = h.car_id
+WHERE
+    c.car_type = '세단' 
+    AND
+    MONTH(h.start_date) = 10
+ORDER BY
+    c.car_id DESC
+;
