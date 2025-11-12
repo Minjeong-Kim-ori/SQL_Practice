d53cb5e178ce4a5495b878910f2b0a8f1153f703
Minjeong-Kim-ori <alswjd240202@gmail.com>
2025-11-12 21:58:25 +0900

    2025-11-12 programmers

---
+++ 프로그래머스_ver2/LV.3/오랜기간보호한동물2.sql
@@ -0,0 +1,12 @@
+SELECT
+    o.animal_id,
+    o.name
+FROM
+    animal_outs AS o
+JOIN
+    animal_ins AS i
+ON
+    o.animal_id = i.animal_id
+ORDER BY
+    DATEDIFF(o.datetime, i.datetime) DESC
+LIMIT 2;
