6d5b0dd10c1094fb68e48ed15d463cbecb8b9c2b
Minjeong-Kim-ori <alswjd240202@gmail.com>
2025-11-18 21:08:14 +0900

    2025-11-18 programmers

---
+++ 프로그래머스_ver2/LV.3/대장균들의자식의수구하기.sql
@@ -0,0 +1,14 @@
+SELECT 
+    e1.id,
+    IFNULL(COUNT(e2.id),0) AS child_count
+FROM
+    ecoli_data AS e1
+LEFT JOIN
+    ecoli_data AS e2
+ON
+    e1.id = e2.parent_id
+GROUP BY
+    e1.id
+ORDER BY
+    e1.id
+;
---
+++ 프로그래머스_ver2/LV.3/물고기종류별대어찾기.sql
@@ -0,0 +1,16 @@
+SELECT
+    i.id, 
+    n.fish_name,
+    i.length
+FROM
+    fish_info AS i
+JOIN
+    fish_name_info AS n
+ON
+    i.fish_type = n.fish_type
+WHERE
+    (i.fish_type,i.length) IN (SELECT fish_type, MAX(length) FROM fish_info GROUP BY fish_type)
+ORDER BY
+    id
+;
+ 
